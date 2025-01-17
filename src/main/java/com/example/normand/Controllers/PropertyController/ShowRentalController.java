5e184c9f9505a0c24e05b5cf8d2051923672a1f5
Sokori <katstomito@gmail.com>
2025-01-17 22:14:12 +0700

    Manager Property and User and PArtly Rental

---
+++ src/main/java/com/example/normand/Controllers/PropertyController/ShowRentalController.java
@@ -0,0 +1,61 @@
+package com.example.normand.Controllers.PropertyController;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ShowRentalController {
+
+    @FXML
+    private TextField viewRentalEnd;
+
+    @FXML
+    private Button viewRentalExit;
+
+    @FXML
+    private TextField viewRentalFee;
+
+    @FXML
+    private TextField viewRentalHost;
+
+    @FXML
+    private TextField viewRentalId;
+
+    @FXML
+    private TextField viewRentalOwner;
+
+    @FXML
+    private TextField viewRentalProperty;
+
+    @FXML
+    private Button viewRentalRemove;
+
+    @FXML
+    private TextField viewRentalStart;
+
+    @FXML
+    private TextField viewRentalStatus;
+
+    @FXML
+    private TextField viewRentalTenant;
+
+    @FXML
+    private Button viewRentalUpdate;
+
+    @FXML
+    void UserExitButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UserRemoveButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void UserSaveButton(ActionEvent event) {
+
+    }
+
+}
